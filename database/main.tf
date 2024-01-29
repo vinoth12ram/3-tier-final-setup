d76fc89e6fedba7f4131c3ae3a0c51b09866100b
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-29 10:37:05 +0000

    Updated main.tf

--- database/main.tf
+++ database/main.tf
@@ -5,16 +5,16 @@ resource "azurerm_mssql_server" "primary" {
     version = var.primary_database_version
     administrator_login = var.primary_database_admin
     administrator_login_password = var.primary_database_password
-    public_network_access_enabled = false
-    minimum_tls_version          = "1.2"
+    public_network_access_enabled = false # Requires for After Scanning
+    minimum_tls_version          = "1.2"  # Requires for After Scanning
 }
 
 resource "azurerm_mssql_database" "db" {
   name              = var.db_name
   server_id         = azurerm_mssql_server.primary.id
-  zone_redundant    = true
+  zone_redundant    = true                # Requires for After Scanning
 
-    lifecycle {
+    lifecycle {                           # Requires for After Scanning
     prevent_destroy = true
   }
 }
