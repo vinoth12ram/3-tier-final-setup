8225f01ad3ca2c01316c69bc740757be54fd76b9
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-29 14:26:13 +0000

    Updated var.tf

--- securitygroup/var.tf
+++ securitygroup/var.tf
@@ -1,6 +1,6 @@
 variable "web-nsg-name" {}
 variable "app-nsg-name" {}
-variable "db_nsg-name" {}
+variable "db-nsg-name" {} # Altered after validate the code
 variable "web_subnet_id" {}
 variable "app_subnet_id" {}
 variable "db_subnet_id" {}
