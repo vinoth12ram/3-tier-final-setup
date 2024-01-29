4a9814353de94ad3f04eb48fe335aab9efc2007d
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-29 13:22:36 +0000

    Updated var.tf

--- monitor/var.tf
+++ monitor/var.tf
@@ -1,5 +1,5 @@
 variable "resource_group" {}
-variable "location" {}
+# variable "location" {} # Commented After Code scanned
 variable "web_vm_id" {}
 variable "app_vm_id" {}
 variable "email_id" {}
