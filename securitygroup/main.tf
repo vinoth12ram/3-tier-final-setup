a15972c06703573b45c23c5a23781289ddfebe6b
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-29 12:17:25 +0000

    Updated main.tf

--- securitygroup/main.tf
+++ securitygroup/main.tf
@@ -9,14 +9,26 @@ resource "azurerm_network_security_group" "web-nsg" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_address_prefix      = "0.0.0.0" #Changed After Scanning
+    source_address_prefix      = "*" #Changed After Scanning
     source_port_range          = "*"
     destination_address_prefix = "*"
-    destination_port_range     = "3389"
+    destination_port_range     = "80"
+  }
+
+    security_rule {
+    name                       = "rule-2"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "*" #Added After Scanning
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+    destination_port_range     = "443"
   }
   
   security_rule {
-    name                       = "rule-2"
+    name                       = "rule-3"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Deny"
