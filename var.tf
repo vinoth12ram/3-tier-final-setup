10fefe2ce29da241e024e57323f63a9f2489ae8f
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-30 11:35:14 +0000

    Updated var.tf

--- var.tf
+++ var.tf
@@ -66,10 +66,10 @@ variable "app_net_id" {
 
 variable "web_subnet_id" {}
 variable "app_subnet_id" {}
-variable "web_host_name"{}
+#variable "web_host_name"{}
 variable "web_username" {}
 variable "web_os_password" {}
-variable "app_host_name"{}
+#variable "app_host_name"{}
 variable "app_username" {}
 variable "app_os_password" {}
 variable "web_availibility_set" {}
