c3a1c00be778349e44feeb54ec03d12eb4bd2d48
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-30 11:17:09 +0000

    Updated var.tf

--- monitor/var.tf
+++ monitor/var.tf
@@ -1,7 +1,7 @@
 variable "resource_group" {}
 # variable "location" {} ## Commented After validate code
-# variable "web_vm_id" {} ## Commented After validate code
-# variable "app_vm_id" {} ## Commented After validate code
+variable "web_vm_id" {} 
+variable "app_vm_id" {} 
 variable "email_id" {}
 variable "vm_count" {
   description = "The number of Windows VM instances to create."
