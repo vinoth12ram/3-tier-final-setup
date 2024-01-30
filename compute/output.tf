d2404e484d010753b0b4b8175d21a3e76071dcb8
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-30 11:46:33 +0000

    Updated output.tf

--- compute/output.tf
+++ compute/output.tf
@@ -1,12 +1,12 @@
-output "web_net_id" {
-  value = azurerm_network_interface.web-net-interface.id
-  description = "Id of web interface in the network"
-}
+#output "web_net_id" {
+#  value = azurerm_network_interface.web-net-interface.id
+#  description = "Id of web interface in the network"
+#}
 
-output "app_net_id" {
-  value = azurerm_network_interface.app-net-interface.id
-  description = "Id of app interface in the network"
-}
+#output "app_net_id" {
+#  value = azurerm_network_interface.app-net-interface.id
+#  description = "Id of app interface in the network"
+#}
 
 output "web_vm_id" {
   value = azurerm_windows_virtual_machine.web-vm.id
