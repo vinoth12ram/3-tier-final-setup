f66183f44d7e4804713ec2e38fbca83de61edfbe
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-31 07:45:48 +0000

    Updated var.tf

--- loadbalancer/var.tf
+++ loadbalancer/var.tf
@@ -5,3 +5,6 @@ variable "lb_pip_name" {}
 variable "backend_addr_pool" {}
 variable "web_net_id" {}
 # variable "app_net_id" {} ## Commented after Validate Code
+variable "vm_count" {
+  description = "The number of Windows VM instances to create."
+}
