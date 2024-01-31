77366a78c9d24b30cab8f2160ea0a1c2f2c9668b
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-31 08:11:50 +0000

    Updated main.tf

--- loadbalancer/main.tf
+++ loadbalancer/main.tf
@@ -43,8 +43,8 @@ resource "azurerm_lb_rule" "this" {
 # Connect VMs to Load Balancer Backend Pool
 
 resource "azurerm_network_interface_backend_address_pool_association" "example" {
-  count               = "${var.vm_count}"
-  network_interface_ids    = [var.web_net_id[count.index]]
+  #count               = "${var.vm_count}"
+  network_interface_id    = var.web_net_id
   ip_configuration_name   = "testconfiguration1"
   backend_address_pool_id = azurerm_lb_backend_address_pool.this.id
 }
