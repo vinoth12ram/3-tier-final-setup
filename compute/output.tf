2650bf6ab8b0a4a3b1601ec51acb70e30e6c6468
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-01-31 04:59:59 +0000

    Updated output.tf

--- compute/output.tf
+++ compute/output.tf
@@ -1,20 +1,20 @@
 output "web_net_id" {
-  value = azurerm_network_interface.web-net-interface.id
+  value = azurerm_network_interface.web-net-interface[*].id
   description = "Id of web interface in the network"
 }
 
 output "app_net_id" {
-  value = azurerm_network_interface.app-net-interface.id
+  value = azurerm_network_interface.app-net-interface[*].id
   description = "Id of app interface in the network"
 }
 
 output "web_vm_id" {
-  value = azurerm_windows_virtual_machine.web-vm.id
+  value = azurerm_windows_virtual_machine.web-vm[*].id
   description = "Id of web vm"
 }
 
 output "app_vm_id" {
-  value = azurerm_windows_virtual_machine.app-vm.id
+  value = azurerm_windows_virtual_machine.app-vm[*].id
   description = "Id of app vm"
 }
 
