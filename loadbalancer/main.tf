836563f53d264be9b252bd9402d97400abe5b993
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-02-01 17:25:56 +0000

    Updated main.tf

--- loadbalancer/main.tf
+++ loadbalancer/main.tf
@@ -46,6 +46,6 @@ resource "azurerm_lb_rule" "this" {
 resource "azurerm_network_interface_backend_address_pool_association" "example" {
   count                   = length(var.web_net_id)
   network_interface_id    = var.web_net_id[count.index]
-  ip_configuration_name   = "testconfiguration1"
+  ip_configuration_name   = var.ipconfig_web_name
   backend_address_pool_id = azurerm_lb_backend_address_pool.this.id
 }
