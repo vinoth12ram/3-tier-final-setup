091a43361caa48a489102ed3bb74fb1801e2b118
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-02-01 10:34:58 +0000

    Update the username parameter, removed the env

--- main.tf
+++ main.tf
@@ -54,10 +54,10 @@ module "compute" {
   web_subnet_id         = module.networking.web_subnet_id
   app_subnet_id         = module.networking.app_subnet_id
   #web_host_name         = "${var.env}-${var.web_host_name}"
-  web_username          = "${var.env}-${var.web_username}"
+  web_username          = var.web_username
   web_os_password       = var.web_os_password
   #app_host_name         = "${var.env}-${var.app_host_name}"
-  app_username          = "${var.env}-${var.app_username}"
+  app_username          = var.app_username
   app_os_password       = var.app_os_password
   ipconfig_app_name     = "${var.env}-${var.ipconfig_app_name}"
   ipconfig_web_name     = "${var.env}-${var.ipconfig_web_name}"
