882e312761d6bf8c172a7e91b4ff83a3539a0475
Mahender Singh <Mahender.Singh@valuemomentum.com>
2024-02-05 09:48:15 +0000

    Removed memory metric alerts

--- monitor/main.tf
+++ monitor/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_monitor_metric_alert" "cpu-alert-app-vms" {
 
 }
 
-resource "azurerm_monitor_metric_alert" "memory-alert-web-vms" {
+/*resource "azurerm_monitor_metric_alert" "memory-alert-web-vms" {
   count               = var.vm_count
   name                = "memory-metricalert-webvms"
   resource_group_name = var.resource_group
@@ -75,28 +75,5 @@ resource "azurerm_monitor_metric_alert" "memory-alert-web-vms" {
   }
 
 
-}
+}*/
 
-resource "azurerm_monitor_metric_alert" "memory-alert-app-vms" {
-  count               = var.vm_count
-  name                = "memory-metricalert-appvms"
-  resource_group_name = var.resource_group
-  scopes              = [var.app_vm_id[count.index]]
-  description         = "Action will be triggered when Memory Threshold is greater than 90."
-
-  criteria {
-
-    metric_namespace = "Microsoft.Compute/virtualMachines"
-    metric_name      = "Available Memory Byte"
-    aggregation      = "Average"
-    operator         = "LessThan"
-    threshold        = 536870912 # Set the threshold in bytes (e.g., 512 MB)
-
-
-  }
- 
-   action {
-    action_group_id = azurerm_monitor_action_group.email-alert.id
-  }
-
-}
